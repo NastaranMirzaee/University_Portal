ebbfb2e922c1ef5e9921e395b08c7f5481d1fd7d
NastaranMirzaee <nstrnm4381@gmail.com>
2023-06-30 15:24:51 +0330

    create model for each app

---
+++ person/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from university.educational.models import *
+
+class Professor(models.Model):
+    personnelcode = models.IntegerField(db_column='personnelCode', primary_key=True)  # Field name made lowercase.
+    nationalcode = models.CharField(db_column='nationalCode', unique=True, max_length=10)  # Field name made lowercase.
+    firstname = models.CharField(db_column='firstName', max_length=45)  # Field name made lowercase.
+    lastname = models.CharField(db_column='lastName', max_length=45)  # Field name made lowercase.
+    phonenumber = models.CharField(db_column='phoneNumber', max_length=12)  # Field name made lowercase.
+    email = models.CharField(max_length=45)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    lasteducationcertificate = models.CharField(db_column='lastEducationCertificate', max_length=45)  # Field name made lowercase.
+    gender = models.CharField(max_length=6)
+    degree = models.CharField(max_length=45)
+    supervisor_flag = models.IntegerField()
+    department_manager_flag = models.IntegerField(db_column='department_ manager_flag')  # Field renamed to remove unsuitable characters.
+    deptno = models.ForeignKey(Department, models.DO_NOTHING, db_column='deptNo', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'professor'
+
+class Student(models.Model):
+    studentno = models.IntegerField(db_column='studentNo', primary_key=True)  # Field name made lowercase.
+    nationalcode = models.CharField(db_column='nationalCode', unique=True, max_length=10)  # Field name made lowercase.
+    firstname = models.CharField(db_column='firstName', max_length=45)  # Field name made lowercase.
+    lastname = models.CharField(db_column='lastName', max_length=45)  # Field name made lowercase.
+    phonenumber = models.CharField(db_column='phoneNumber', max_length=12)  # Field name made lowercase.
+    email = models.CharField(max_length=45)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    entranceyear = models.IntegerField(db_column='entranceYear')  # Field name made lowercase.
+    gender = models.CharField(max_length=6)
+    birthdate = models.DateField(blank=True, null=True)
+    nationality = models.CharField(max_length=45, blank=True, null=True)
+    deptno = models.ForeignKey(Department, models.DO_NOTHING, db_column='deptNo', blank=True, null=True)  # Field name made lowercase.
+    supervisor = models.ForeignKey(Professor, models.DO_NOTHING, blank=True, null=True)
+    gpa = models.FloatField(blank=True, null=True)
+    passed_credit = models.IntegerField(blank=True, null=True)
+    balance = models.IntegerField(blank=True, null=True)
+    taken_credit = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'student'
---
+++ service/models.py
@@ -0,0 +1,95 @@
+from university.person.models import *
+
+class Food(models.Model):
+    food_id = models.IntegerField(primary_key=True)
+    food_name = models.CharField(unique=True, max_length=45)
+    meal = models.CharField(max_length=45)
+    price = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'food'
+
+class FoodReservation(models.Model):
+    food = models.OneToOneField(Food, models.DO_NOTHING, primary_key=True)  # The composite primary key (food_id, studentNo) found, that is not supported. The first column is selected.
+    studentno = models.ForeignKey('Student', models.DO_NOTHING, db_column='studentNo', to_field='studentno')  # Field name made lowercase.
+    food_reservation_date = models.DateField()
+
+    class Meta:
+        managed = False
+        db_table = 'food_reservation'
+        unique_together = (('food', 'studentno'),)
+
+class Sport(models.Model):
+    s_id = models.IntegerField(primary_key=True)
+    price = models.IntegerField()
+    capacity = models.CharField(max_length=45)
+    name = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'sport'
+
+class LeisureClass(models.Model):
+    leisure_class = models.OneToOneField('Sport', models.DO_NOTHING, primary_key=True)
+    l_from = models.DateField(db_column='L_from', blank=True, null=True)  # Field name made lowercase.
+    l_to = models.DateField(db_column='L_to', blank=True, null=True)  # Field name made lowercase.
+    l_time = models.TimeField(db_column='L_time', blank=True, null=True)  # Field name made lowercase.
+    day1 = models.CharField(max_length=45, blank=True, null=True)
+    day2 = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'leisure_class'
+
+class Pool(models.Model):
+    pool = models.OneToOneField('Sport', models.DO_NOTHING, primary_key=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'pool'
+
+class PoolReservation(models.Model):
+    studentno = models.OneToOneField('Student', models.DO_NOTHING, db_column='studentNo', primary_key=True)  # Field name made lowercase. The composite primary key (studentNo, pool_id) found, that is not supported. The first column is selected.
+    pool = models.ForeignKey(Pool, models.DO_NOTHING)
+    from_field = models.TimeField(db_column='from', blank=True, null=True)  # Field renamed because it was a Python reserved word.
+    to = models.TimeField(blank=True, null=True)
+    date_pool = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'pool_reservation'
+        unique_together = (('studentno', 'pool'),)
+
+class RegisterLeisureClass(models.Model):
+    studentno = models.OneToOneField('Student', models.DO_NOTHING, db_column='studentNo', primary_key=True)  # Field name made lowercase. The composite primary key (studentNo, leisure_class_id) found, that is not supported. The first column is selected.
+    leisure_class = models.ForeignKey(LeisureClass, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'register_leisure_class'
+        unique_together = (('studentno', 'leisure_class'),)
+
+class Research(models.Model):
+    research_code = models.IntegerField(primary_key=True)
+    author = models.CharField(max_length=45, blank=True, null=True)
+    title = models.CharField(max_length=45, blank=True, null=True)
+    book_flag = models.IntegerField(blank=True, null=True)
+    paper_flag = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'research'
+
+class ResearchReservation(models.Model):
+    research_code = models.OneToOneField(Research, models.DO_NOTHING, db_column='research_code', primary_key=True)  # The composite primary key (research_code, studentNo) found, that is not supported. The first column is selected.
+    studentno = models.ForeignKey('Student', models.DO_NOTHING, db_column='studentNo', to_field='studentno')  # Field name made lowercase.
+    r_from = models.DateField(blank=True, null=True)
+    r_to = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'research_reservation'
+        unique_together = (('research_code', 'studentno'),)
+
